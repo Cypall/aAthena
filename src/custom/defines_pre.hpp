e520cc6e43ae371785c553d3a489631e0020fef2
Cypall <cypall.mail@gmail.com>
2024-11-18 19:02:53 +0700

    aAthena

---
+++ src/custom/defines_pre.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef CONFIG_CUSTOM_DEFINES_PRE_HPP
+#define CONFIG_CUSTOM_DEFINES_PRE_HPP
+
+/**
+ * Athena configuration file
+ * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
+ **/
+// Enable Renewal
+#define PACKETVER 20211103
+#define PACKETVER_RE
+
+#endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
